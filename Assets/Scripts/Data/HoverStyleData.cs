882e3e06c9daba27901f84078a083e56f98d7a54
Guthen <114919245+arkaht@users.noreply.github.com>
2023-03-27 16:42:41 +0200

    [added] menu tabs in-scene

---
+++ Assets/Scripts/Data/HoverStyleData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu( menuName = "Data/HoverStyleData" )]
+public class HoverStyleData : ScriptableObject
+{
+	public Color DefaultBackgroundColor = Color.white,
+				 DefaultTextColor = Color.black;
+	public Color HoverBackgroundColor = Color.black,
+				 HoverTextColor = Color.white;
+}
---
+++ Assets/Scripts/UI/UIHoverStyle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIHoverStyle : MonoBehaviour,
+							IPointerEnterHandler, IPointerExitHandler
+{
+	public bool IsHovered
+	{
+		get => isHovered;
+		set
+		{
+			isHovered = value;
+
+			background.color = isHovered ? style.HoverBackgroundColor : style.DefaultBackgroundColor;
+			tmpText.color = isHovered ? style.HoverTextColor : style.DefaultTextColor;
+		}
+	}
+	private bool isHovered = false;
+
+	[SerializeField]
+	private HoverStyleData style;
+
+	[SerializeField]
+	private Image background;
+	[SerializeField]
+	private TMPro.TextMeshProUGUI tmpText;
+
+	public void OnPointerEnter( PointerEventData data ) => IsHovered = true;
+	public void OnPointerExit( PointerEventData data ) => IsHovered = false;
+
+	void Start()
+	{
+		IsHovered = false;
+	}
+
+	void OnValidate()
+	{
+		if ( background == null )
+			background = GetComponentInChildren<Image>();
+		if ( tmpText == null )
+			tmpText = GetComponentInChildren<TMPro.TextMeshProUGUI>();
+
+		if ( style != null )
+			IsHovered = false;
+	}
+}
