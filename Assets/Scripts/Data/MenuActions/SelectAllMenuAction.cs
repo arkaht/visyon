ea2bad0efad03324cd17fbde2428aab7c3bec74f
Guthen <114919245+arkaht@users.noreply.github.com>
2023-03-30 18:49:46 +0200

    [added] select all menu action

---
+++ Assets/Scripts/Data/MenuActions/SelectAllMenuAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu( menuName = "Data/MenuAction/SelectAll" )]
+public class SelectAllMenuAction : MenuAction
+{
+	public override void Execute( Blueprinter blueprint ) 
+	{
+		foreach ( UISelectable selectable in FindObjectsOfType<UISelectable>() )
+			blueprint.AddToSelection( selectable );
+	}
+}
