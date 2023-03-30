53096036abd7066f4e67798b9e038358d58a6f64
Guthen <114919245+arkaht@users.noreply.github.com>
2023-03-30 18:39:49 +0200

    [added] official wiki url menu action

---
+++ Assets/Scripts/Data/MenuActions/URLMenuAction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu( menuName = "Data/MenuAction/URL" )]
+public class URLMenuAction : MenuAction
+{
+	public string URL;
+
+	public override void Execute( Blueprinter blueprint ) 
+	{
+		Application.OpenURL( URL );
+	}
+}
