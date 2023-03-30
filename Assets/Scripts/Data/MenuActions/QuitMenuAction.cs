3711da11c2de17f1bf007e84504ef8956312c701
Guthen <114919245+arkaht@users.noreply.github.com>
2023-03-30 18:24:30 +0200

    [added] quit menu action

---
+++ Assets/Scripts/Data/MenuActions/QuitMenuAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu( menuName = "Data/MenuAction/Quit" )]
+public class QuitMenuAction : MenuAction
+{
+	public override void Execute( Blueprinter blueprint ) 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
