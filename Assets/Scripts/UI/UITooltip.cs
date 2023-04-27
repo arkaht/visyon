3dae44de15a107093eb5fed036f4775d2ba02047
Guthen <114919245+arkaht@users.noreply.github.com>
2023-04-27 21:19:58 +0200

    [added] tooltip

---
+++ Assets/Scripts/UI/UITooltip.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Visyon.Core;
+
+public class UITooltip : MonoBehaviour
+{
+	public static UITooltip Instance { get; private set; }
+
+	[SerializeField]
+	private Image highlightImage;
+	[SerializeField]
+	private TMP_Text textTMP;
+
+	[SerializeField]
+	private Color highlightColor, disabledColor;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+	void Start()
+	{
+		Hide();
+	}
+
+	public void Show( Vector2 pos, string name, string text = "" )
+	{
+		gameObject.SetActive( true );
+
+		Move( pos );
+
+		//  change text
+		if ( text == string.Empty )
+			textTMP.text = TextMarkup.AsHeader( 4, name );
+		else
+			textTMP.text = $"{TextMarkup.AsHeader( 4, name )}\n{TextMarkup.AsItalic( text )}";
+	}
+	public void Hide()
+	{
+		gameObject.SetActive( false );
+	}
+	public void SetHighlight( bool is_highlight )
+	{
+		Color color = is_highlight ? highlightColor : disabledColor;
+		highlightImage.color = color;
+		textTMP.color = color;
+	}
+
+	public void Move( Vector2 pos )
+	{
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/UI/UITooltipChanger.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UITooltipChanger : MonoBehaviour,
+								IPointerEnterHandler, IPointerMoveHandler, IPointerExitHandler
+{
+	public string Name = "Name";
+	public string Text = "Tooltip";
+
+	[SerializeField]
+	private Button button;
+
+	private UITooltip currentTooltip;
+
+	public bool IsInteractable()
+	{
+		if ( button != null )
+			return button.interactable;
+
+		return true;
+	}
+
+	public void OnPointerEnter( PointerEventData data )
+	{
+		currentTooltip = UITooltip.Instance;
+		if ( currentTooltip == null ) return;
+
+		currentTooltip.Show( data.position, Name, Text );
+	}
+
+	public void OnPointerMove( PointerEventData data )
+	{
+		if ( currentTooltip == null ) return;
+		currentTooltip.Move( data.position );
+	}
+
+	public void OnPointerExit( PointerEventData data )
+	{
+		if ( currentTooltip == null ) return;
+		currentTooltip.Hide();
+		currentTooltip = null;
+	}
+
+	void Update()
+	{
+		if ( currentTooltip == null ) return;
+		currentTooltip.SetHighlight( IsInteractable() );
+	}
+
+	void OnDisable()
+	{
+		OnPointerExit( null );
+	}
+}
